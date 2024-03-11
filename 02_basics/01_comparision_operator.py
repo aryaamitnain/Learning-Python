97edf241aa8efdc8a99b00db24f76b3be6ee7935
Amit Nain <aryaamitnain08@gmail.com>
2024-03-11 07:28:50 +0000

    comparision operators

---
+++ 02_basics/01_comparision_operator.py
@@ -0,0 +1,11 @@
+# x = 10>7 
+# print(x)
+# print(type(x))
+
+# x = 34>=33 and 25>=33 and 45>=33
+# print(x)
+
+# or and and logical operator
+
+x = (23>=33 or 45>=33) and 35>=33 and 89>=33
+print(x)
