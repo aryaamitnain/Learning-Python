f35e8fc11e6f6b42f654bc1edfd126844fd889de
Amit Nain <aryaamitnain08@gmail.com>
2024-03-13 11:57:29 +0000

    identity operators

---
+++ 02_basics/03_identity_operator.py
@@ -0,0 +1,21 @@
+# There are two indentity operators {1. (is and) 2. (is not)}
+
+# x = 5 # python allot the storage to the value not to variables
+# y = 6
+# print(x is not y)
+
+# z = x is y 
+# z = x is not y
+# print(id(x))
+# print(id(y))
+# print(z)
+
+# ++++++++++++++++++++++++++++++++ Membership operators ++++++++++++++++++++
+
+# 1. in 2. not in 
+
+x = 'a'
+y = "amit nain"
+
+z = x in y
+print(z)
