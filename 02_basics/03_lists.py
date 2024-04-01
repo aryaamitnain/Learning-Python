d637fda392b9ca3bcb9359f20925b85d7019ec59
Amit Nain <aryaamitnain08@gmail.com>
2024-04-01 05:29:45 +0000

    lists

--- 02_basics/03_lists.py
+++ 02_basics/03_lists.py
@@ -1,10 +1,21 @@
-lis = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lis = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Amit", "gaurav"]
 
 # print(lis)
 # print(lis[1])
+# print(len(lis))
 
 # print(lis[: : -1])
 # print(lis[2]*5)
 
-lis[1]= "amit"
-print(lis)
+# lis[4]= "amit"
+# print(lis)
+
+# for x in lis:
+#     print(x)
+
+if "Amit" in lis:
+    print(f"yes")
+else:
+    print(f"no")
+
+
