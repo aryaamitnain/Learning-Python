4ed37d0e1b018a6770c0bb188756669e8ce9d157
Amit Nain <aryaamitnain08@gmail.com>
2024-04-02 10:19:16 +0000

    lists-methods

--- 02_basics/03_lists.py
+++ 02_basics/03_lists.py
@@ -1,11 +1,12 @@
-lis = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Amit", "gaurav"]
-
-# print(lis)
+lis = [1, 2, 3, 4, 12, 6, 7, 8, 9]
+lis.sort()
+print(lis)
 # print(lis[1])
 # print(len(lis))
 
 # print(lis[: : -1])
 # print(lis[2]*5)
+
 
 # lis[4]= "amit"
 # print(lis)
@@ -13,9 +14,20 @@ lis = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Amit", "gaurav"]
 # for x in lis:
 #     print(x)
 
-if "Amit" in lis:
-    print(f"yes")
-else:
-    print(f"no")
+# if "Amit" in lis:
+#     print(f"yes")
+# else:
+#     print(f"no")
 
+#  adding the items in the lists ------------------------
 
+thisList = ["apple", "mango", "orange"]
+thisList.append("grapes")
+thisList.insert(2, "banana")
+
+print(thisList)
+
+# thisList.remove("apple")
+thisList.pop(1)
+
+print(thisList)
