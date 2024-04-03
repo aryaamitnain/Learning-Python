96052110ceab74e45d0b3acce2025fee16b62215
Amit Nain <aryaamitnain08@gmail.com>
2024-04-03 04:58:51 +0000

    sets

---
+++ 02_basics/06_sets.py
@@ -0,0 +1,15 @@
+# set is unordered, unindexed and not allowed duplicate items
+
+thisset = {"apple", "mango", "banana", 3, 5, "amit", "apple"}
+
+# print(type(thisset))
+# print(thisset)
+
+# for i in thisset:
+#     print(i)
+
+# thisset.add("adding this")
+# print(thisset)
+
+thisset.update(["this", 77])
+print(thisset)
