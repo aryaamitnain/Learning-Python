39f36de6b558f5e1920941ad67a9a7ad444244bb
Amit Nain <aryaamitnain08@gmail.com>
2024-04-05 06:04:56 +0000

    contitions

---
+++ 03_basics/02_condition.py
@@ -0,0 +1,9 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+if a < b:
+    print(f"yes {a} is less than {b}")
+elif a==b:
+    print({f"{a} is equal to {b}"})
+else:
+    print(f"no {a} is not less than {b}")
