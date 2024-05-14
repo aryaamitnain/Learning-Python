c6c819ad3a8fdabc4b00bc8c7ec83fdbc7484bb8
Amit Nain <aryaamitnain08@gmail.com>
2024-05-14 07:21:58 +0000

    for loop

--- 04_loops/01_whileloop.py
+++ 04_loops/01_whileloop.py
@@ -1,4 +1,36 @@
-i = 1
-while i < 6:
-    print(i)
-    i +=1
+# i = 1
+# while i<=10:
+#     print(i, end=" ")
+#     i +=1
+
+# +++++++++++++ // Exercise // +++++++++++++
+
+# # get user input
+# no1 = int(input("Enter where to start: "))
+# no2 = int(input("Enter where to stop: "))
+
+# # create while loop
+
+# while no1 <=no2:
+#     print(f"{no1}", end=" ")
+#     no1+=1
+
+
+# ++++++++++++++++++ // exercise 2 print tables // +++++++++++++++++
+
+# get user input
+starting = int(input("Enter any number from where to start: "))
+ending = int(input("Enter any number to where to stop: "))
+
+
+while starting <= ending:
+    i = 1
+    while i<=10:
+        print(f"{starting*i}", end=" ")
+        i +=1
+    print()
+    starting +=1 
+
+
+
+
---
+++ 04_loops/02_forloop.py
@@ -0,0 +1,3 @@
+i = 2
+for i in range(7):
+    print(i)
