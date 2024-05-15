e36209964ff300fae525e0f5f2a93b2afcbe74a6
Amit Nain <aryaamitnain08@gmail.com>
2024-05-15 05:09:52 +0000

    for loop

--- 04_loops/02_forloop.py
+++ 04_loops/02_forloop.py
@@ -1,3 +1,13 @@
-i = 2
-for i in range(7):
-    print(i)
+# i = 2
+# for i in range(7):
+#     print(i)
+
+# for i in range (5):
+#     print(i, end=" ")
+
+adj = ["red", "blue", "green"]
+fruits = ["apple", "mango", "pineapple"]
+
+for x in adj:
+    for y in fruits:
+        print(x,y)
